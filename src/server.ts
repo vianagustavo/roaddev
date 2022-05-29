19643230c0f412208d06289afadf448235c529ec
Gustavo Viana <gustavofvv@gmail.com>
2022-05-29 15:14:12 +0000

    feat: add docker compose

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import app from "./app";
 
 AppDataSource.initialize()
   .then(() => {
-    app.listen(process.env.PORT, () => console.log("Server is running!"));
+    app.listen(process.env.PORT, () =>
+      console.log(`Server is running on port ${process.env.PORT}!`)
+    );
   })
   .catch((error) => console.log(error));
